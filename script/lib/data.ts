307fefd8ac9bb17f30cf887eaf5722515433f9f1
Ivan Herman <ivan@ivan-herman.net>
2025-03-24 14:09:49 +0100

    Improve tf view (in separate sections)

--- script/lib/data.ts
+++ script/lib/data.ts
@@ -1,6 +1,7 @@
-import { MiniDOM } from './minidom.ts';
-import { filenameToTitle } from "./params.ts";
-import * as fs     from 'node:fs/promises';
+import { MiniDOM }              from './minidom.ts';
+import { taskForces, location } from "./params.ts";
+import * as fs                  from 'node:fs/promises';
+import * as path                from 'node:path';
 
 type FileName = string;
 
@@ -8,10 +9,10 @@ type FileName = string;
 interface DisplayedData {
     /** The File name of the minutes */
     fname: FileName;
+    /** The final path to the minutes, to be used for links */
+    location: FileName
     /** Date of the minutes */
     date: Date;
-    /** Title of the meeting */
-    title: string
     /** Array of TOC entries (can be HTML) */
     toc: string[];
     /** Array of Resolution entries entries (can be HTML) */
@@ -19,9 +20,12 @@ interface DisplayedData {
 }
 
 /** The data regrouped per years */
-export type GroupedData = Map<number, DisplayedData[]>;
+export type GroupedData   = Map<number, DisplayedData[]>;
+export type GroupedTFData = Map<string, GroupedData>;
 
-/** Files names that must be ignored in the directory of minutes, if there are present */
+/** 
+ * Files names that must be ignored in the directory of minutes, if there are present 
+ */
 const ignoredFiles: string[] = ["index.html", "resolutions.html"];
 
 /**
@@ -36,27 +40,6 @@ async function getMinutes(directory: string): Promise<FileName[]> {
     return (await fs.readdir(directory))
         .filter(file => !ignoredFiles.includes(file))   
         .map(file => `${directory}/${file}`);
-}
-
-/**
- * Get the title of the meeting from the file name.
- * 
- * @param fname 
- * @returns 
- */
-function getMeetingTitle(fname: FileName): string {
-    // Get the html part off the file name
-    const baseName = fname.split(".html")[0];
-
-    // Get the last part of the file name which may be the meeting category
-    const parts: string[] = baseName.split("-");
-    const name: string = parts.pop() ?? "default";
-    if (name === undefined) {
-        // Although this may not happen, better keep the TS compiler happy
-        return filenameToTitle["default"];
-    } else {
-        return filenameToTitle[name] ?? filenameToTitle["default"];
-    }
 }
 
 /**
@@ -75,7 +58,7 @@ async function getAllData(minutes: FileName[]): Promise<DisplayedData[]> {
         const cleanupData = (nav: string): string => {
             return nav
                 // References should not be relative
-                .replace(/href="#/g, `target="_blank" href="${fname}#`)
+                .replace(/href="#/g, `target="_blank" href="${path.join(location, path.basename(fname))}#`)
                 ;
         };
 
@@ -104,12 +87,14 @@ async function getAllData(minutes: FileName[]): Promise<DisplayedData[]> {
         const date_title :string | null | undefined  = content.querySelector("header h2:first-of-type")?.textContent;
         const date       = new Date(date_title ?? "1970-01-01");
 
+        // console.log(`Processing ${fname}, ${path.basename(fname)}, ${path.join(location, path.basename(fname))}`);
+
         return {
-            fname : fname,
-            date  : date,
-            title : `${getMeetingTitle(fname)} Meeting`,
-            toc   : extractListEntries(fname, content, "#toc ol li"),  
-            res   : extractListEntries(fname, content, "#ResolutionSummary ol li"), 
+            fname    : fname,
+            location : path.join(location, path.basename(fname)),
+            date     : date,
+            toc      : extractListEntries(fname, content, "#toc ol li"),  
+            res      : extractListEntries(fname, content, "#ResolutionSummary ol li"), 
         };
     }
 
@@ -156,4 +141,43 @@ export async function getGroupedData(directory: string): Promise<GroupedData> {
     // For each of the minutes, get the content.
     const display: DisplayedData[] = await getAllData(minutes);
     return groupDisplayedDataByYear(display);
+}
+
+/**
+ * Take care of task forces and group the data accordingly.
+ * 
+ * @param groupedData 
+ * @returns 
+ */
+export async function getTFGroupedData(directory: string): Promise<GroupedTFData> {
+    const groupedData = await getGroupedData(directory);
+    const groups: GroupedTFData = new Map<string, GroupedData>();
+
+    for (const tf of Object.keys(taskForces)) {
+        const group = new Map<number, DisplayedData[]>();
+        let groupEmpty = true;
+        for (const [year, data] of groupedData) {
+            const filteredData = data.filter((entry: DisplayedData): boolean =>  {
+                const baseName = entry.fname.split(".html")[0];
+                const parts = baseName.split("-");
+                // the convention is that the last part is the task force identifier
+                // which follows the the date string
+                if (parts.length === 3 && tf === "default") {
+                    // if the task force is not specified, we take the default
+                    return true;
+                } else {
+                    // if the task force is specified, we take that one
+                    return parts[parts.length - 1] === tf;  
+                }
+            })
+            if (filteredData.length > 0) {
+                group.set(year, filteredData);
+                groupEmpty = false;
+            }
+        }
+        if (!groupEmpty) {
+            groups.set(tf, group);
+        }
+    }
+    return groups;
 }
